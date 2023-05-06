bc9ee112ec64545d4ff197e050c92c55a64a9a65
Alina <1624232376@qq.com>
2023-05-06 10:22:23 +0800

    delete

--- 2.4GHz radar/readme.md
+++
@@ -1,5 +0,0 @@
-### main.mlx
-comparison of three phase extraction methods  
-use digital filter for vital signs detection 
-### Rawdata folder  
-collected raw data files
--- 77GHz radar/configure_file/capture.lua
+++
@@ -1,6 +0,0 @@
---此脚本用于控制采集过程的开始和结束
-ar1.CaptureCardConfig_StartRecord(adc_data_path, 1)--DCA1000开始收集
-ar1.StartFrame()--AWR1642开始采集
-RSTD.Sleep(23400)--单位：毫秒。sleep时长大于采集时长，目的是给mmWave Studio采集完后的其他操作给出时间
-ar1.StopFrame()--AWR1642停止采集
-ar1.CaptureCardConfig_StopRecord()--DCA1000停止收集
